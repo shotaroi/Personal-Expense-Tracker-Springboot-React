0735ed2beb4f7cd45c34b30b3af8b7a66380b17c
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-05-12 06:33:33 +0200

    Add form functionality to App component for creating new expenses, including state management and submission handling.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,12 @@ function App() {
   const [expenses, setExpenses] = useState([]);
   const [loading, setLoading] = useState(true);
   const[error, setError] = useState('');
+  const [form, setForm] = useState({
+    title: '',
+    amount: '',
+    category: '',
+    date: '',
+  })
 
   useEffect(() => {
     fetch("http://localhost:8080/api/expenses")
@@ -19,6 +25,37 @@ function App() {
     .finally(() => setLoading(false));
   }, []);
 
+  function handleChange(event) {
+    const { name, value } = event.target
+    setForm({ ...form, [name]: value })
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    fetch('http://localhost:8080/api/expenses', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        ...form,
+        amount: Number(form.amount),
+      }),
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Failed to create expense');
+      }
+      return response.json();
+    })
+    .then(newExpenses => {
+      setExpenses([...expenses, newExpenses]);
+      setForm({ title: '', amount: '', category: '', date: ''})
+    })
+    .catch(error => setError(error.message))
+  }
+
   return (
     <>
      <main>
@@ -26,6 +63,14 @@ function App() {
 
       {loading && <p>Loading expenses...</p>}
       {error && <p>{error}</p>}
+
+      <form onSubmit={handleSubmit}>
+        <input name='title' value={form.title} onChange={handleChange} placeholder='Title' />
+        <input name='amount' value={form.amount} onChange={handleChange} placeholder='Amount' type='number' step='0.01' />
+        <input name='category' value={form.category} onChange={handleChange} placeholder='Category' />
+        <input name='date' value={form.date} onChange={handleChange} type='date' />
+        <button type='submit'>Add Expense</button>
+      </form>
 
       {!loading && !error && (
         <ul>
